fe26f1fb6a48cbeb4691698d21aefe9245b44cfd
David Leitner <david@leitner.io>
2018-12-12 23:08:00 +0100

    Add touch-ripple to content-row

--- src/components/Content/ContentRow.tsx
+++ src/components/Content/ContentRow.tsx
@@ -1,12 +1,18 @@
-import { withStyles, Theme, StyledComponentProps } from '@material-ui/core';
+import { withStyles, Theme, StyledComponentProps, ButtonBase } from '@material-ui/core';
 import React from 'react';
+import { StyleRules } from '@material-ui/core/styles';
 
 const content: React.SFC<StyledComponentProps> = ({ children, classes = {} }) => (
-  <div className={classes.header}>{children}</div>
+  <ButtonBase className={classes.header}>{children}</ButtonBase>
 );
 
-const style = (theme: Theme) => ({
+const style = (theme: Theme): StyleRules => ({
   header: {
+    // reset button base
+    display: 'block',
+    width: '100%',
+    textAlign: 'left',
+
     fontSize: theme.typography.body2.fontSize,
     backgroundColor: theme.palette.common.white,
     padding: theme.spacing.unit * 2,
